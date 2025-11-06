f0e4af41324f7d18be62aae33743ca1a5dc282b9
Daniel Aparisi <daniel.aparisi.lozano@gmail.com>
2025-11-06 21:30:27 +0100

    Strating with expo router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,24 @@
-import {View, Text} from "react-native"
-import { Slot } from "expo-router"
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 
-export default function Layout () {
-    return(
-        <View className=" flex-1 bg-black items-center">
-            <Slot />
-        </View>
-    )
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hide();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <Stack />;
 }
--- app/index.tsx
+++
@@ -1,6 +0,0 @@
-import Main from '../App'
-
-
-export default function Index () {
-    return <Main />
-}
